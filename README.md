b76ed93013b1d465c075aa1cac2b7b4969504b55
lemoscaio <caio_lemos94@hotmail.com>
2022-03-05 04:59:04 -0300

    Conserta lógica e faz o site funcionar

--- README.md
+++
@@ -1,1 +0,0 @@
-# mini-hackathon-driven
--- script.js
+++ script.js
@@ -5,93 +5,18 @@ let infoUsers = []
 
 async function getInfoUsersLocation(callback) {
     if ("geolocation" in navigator) {
-        await navigator.geolocation.getCurrentPosition((objetoPosition) => {
+        navigator.geolocation.getCurrentPosition((objetoPosition) => {
             latitude = objetoPosition.coords.latitude;
             longitude = objetoPosition.coords.longitude
-            console.log('testando');
             callback()
         })
-      } else {
-        console.log('teste');
-      }
-      
-}
-
-let objetoTemp = {
-    "data": {
-        "coord": {
-            "lon": -43.3009,
-            "lat": -22.8441
-        },
-        "weather": [{
-            "id": 803,
-            "main": "Clouds",
-            "description": "nublado",
-            "icon": "04n"
-        }],
-        "base": "stations",
-        "main": {
-            "temp": 26.29,
-            "feels_like": 26.29,
-            "temp_min": 24.84,
-            "temp_max": 27.99,
-            "pressure": 1012,
-            "humidity": 77
-        },
-        "visibility": 10000,
-        "wind": {
-            "speed": 3.09,
-            "deg": 50
-        },
-        "clouds": {
-            "all": 75
-        },
-        "dt": 1646430225,
-        "sys": {
-            "type": 1,
-            "id": 8376,
-            "country": "BR",
-            "sunrise": 1646383837,
-            "sunset": 1646428775
-        },
-        "timezone": -10800,
-        "id": 3464374,
-        "name": "Duque de Caxias",
-        "cod": 200
-    },
-    "status": 200,
-    "statusText": "OK",
-    "headers": {
-        "content-length": "475",
-        "content-type": "application/json; charset=utf-8"
-    },
-    "config": {
-        "transitional": {
-            "silentJSONParsing": true,
-            "forcedJSONParsing": true,
-            "clarifyTimeoutError": false
-        },
-        "transformRequest": [
-            null
-        ],
-        "transformResponse": [
-            null
-        ],
-        "timeout": 0,
-        "xsrfCookieName": "XSRF-TOKEN",
-        "xsrfHeaderName": "X-XSRF-TOKEN",
-        "maxContentLength": -1,
-        "maxBodyLength": -1,
-        "headers": {
-            "Accept": "application/json, text/plain, */*"
-        },
-        "method": "get",
-        "url": "https://api.openweathermap.org/data/2.5/weather?lat=-22.8441034&lon=-43.3009435&units=metric&lang=pt_br&appid=0abcf8b7e32777623a9f64fe87e6a749"
-    },
-    "request": {}
+    } else {
+        console.log("Não conseguimos buscar a localização do usuário. Favor inserir manualmente");
+    }
 }
 
 function getWeatherByLocation() {
+    
     let URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&lang=pt_br&appid=${API_KEY}`
     axios({
         method: 'get',
@@ -100,59 +25,96 @@ function getWeatherByLocation() {
         infoUsers = response
         console.log(infoUsers);
         renderInfosHTML()
-     
+
     })
 }
 
-function renderInfosHTML() {
+function renderInfosHTML(cidade) {
     const container = document.querySelector('.modal-body')
-    const cityName = document.querySelector('#exampleModalLabel')
-    container.innerHTML =
-        `
-        <section class="container-left">
-            <div class="gif-weather">
-                <img src="gifs/${infoUsers.data.weather[0].icon}" alt="">
-            </div>
-        </section>
+    const cityName = document.querySelector('.modal-title')
 
-        <section class="weather-info container-right">
+    const descricaoTempo = infoUsers.data.weather[0].description.replace(/^\w/, (letra) => letra.toUpperCase());
 
-            <div class="weather-summary">
-                <h2 class="temperature-number">${infoUsers.data.main.temp}º C</h2>
-                <div class="weather-icon">
-                    <img src='http://openweathermap.org/img/wn/${infoUsers.data.weather[0].icon}@2x.png'/>
-                </div>
-            </div>
+    const template = `
+    <section class="container-left">
+        <div class="gif-weather">
+            <img class="gif-weather-source" src="gifs/${infoUsers.data.weather[0].icon}.gif" alt="GIF of the weather">
+        </div>
+    </section>
 
-            <div class="weather-other-info d-flex justify-content-space-between">
-                <p class="weather__description">${infoUsers.data.weather[0].description}</p>
-                <P class="teperature-feelslike">Sensação: ${infoUsers.data.main.feels_like}°</P>
+    <section class="weather-info container-right">
+
+        <div class="weather-summary d-flex justify-content-center align-items-center">
+            <h2 class="temperature-number display-7 fw-bold">${infoUsers.data.main.temp}º C</h2>
+            <div class="weather-icon">
+                <img src='http://openweathermap.org/img/wn/${infoUsers.data.weather[0].icon}@2x.png'/>
             </div>
-            <div class="weather__wind">
-                <p class="weather__wind-degrees">Velocidade: ${infoUsers.data.wind.speed} Km/h</p>
-                <p class="weather__wind-degrees">Direção: ${infoUsers.data.wind.deg}º</p>
-            </div>
-        </section>
-    `
+            <div class="weather-max-min">
+                                <p class="fw-light mb-1">Máx: ${infoUsers.data.main.temp_max} C</p>
+                                <p class="fw-lighter mb-1">Min: ${infoUsers.data.main.temp_min} C</p>
+                            </div>
+        </div>
+
+        <div class="weather-other-info d-flex justify-content-evenly ml-4 ml-4">
+            <p class="weather__description">${descricaoTempo}</p>
+            <P class="teperature-feelslike">Sensação: ${infoUsers.data.main.feels_like}°</P>
+        </div>
+        <div class="weather__wind">
+        <p class="weather__wind-header text-start mt-3 mb-0">Vento:</p>
+        <div class="weather__wind-fields d-flex">
+            <p class="weather__wind-degrees me-4 mb-0">Velocidade: ${infoUsers.data.wind.speed} Km/h</p>
+        <p class="weather__wind-degrees mb-0">Direção: ${infoUsers.data.wind.deg}º</p>
+        </div>
+        <div>
+        
+    </section>
+`
+
+    container.innerHTML = template
+
+
+    cityName.textContent = infoUsers.data.name
+
+    var myModal = new bootstrap.Modal(document.getElementById('exampleModal'))
+    myModal.show()
 }
-function getInfosLocationByTheUser() {
-    let postalCode = document.querySelector('.postalCode').value
-    let siglaPais = document.querySelector('.siglasPais').value
-    console.log(postalCode,siglaPais);
-    const URL= `http://api.openweathermap.org/geo/1.0/zip?zip=${postalCode},${siglaPais}&appid=${API_KEY}`
-                //  http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}
- 
+
+function getInfosLocationByTheUser(cidade) {
+
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&lang=pt_br&appid=${API_KEY}`
+
     axios({
-        method:'get',
+        method: 'get',
         url: URL
-    }).then(response =>{
+    }).then(response => {
         infoUsers = response
         console.log(infoUsers);
-        renderInfosHTML()
-    })
+        renderInfosHTML(cidade)
+    }).catch(informarErro)
+}
+
+function getFormsValue(event) {
+    event.preventDefault()
+    const inputCidade = document.querySelector(".city-name")
+    const cidade = inputCidade.value
+
+    if (cidade.length > 0) {
+        getInfosLocationByTheUser(cidade)
+    } else(
+        alert("Insira o nome de uma cidade")
+    )
+
+}
+
+function informarErro() {
+    const elementoErro = document.createElement("div")
+    const mensagemErro = `<p class="text-danger">Vish... A busca não retornou resultado.</p>`
+
+    elementoErro.innerHTML = mensagemErro
+    const elementoPai = document.querySelector(".form")
+    const botaoEl = document.querySelector(".buscar-cidade")
+
+    elementoPai.insertBefore(elementoErro, botaoEl)
 }
 
 getInfoUsersLocation(getWeatherByLocation)
-// renderInfosHTML()
-
-// getInfoUsersLocation(getWeatherByLocation)
